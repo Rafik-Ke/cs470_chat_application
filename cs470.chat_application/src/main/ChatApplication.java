da11d3c5200379d9f8fd84c74d96e75b4164d2d1
rkeshishians <rafik_ke@ymail.com>
2016-04-21 19:07:54 -0700

    TestServer and TestClient running constantly to test the server

--- cs470.chat_application/src/main/ChatApplication.java
+++ cs470.chat_application/src/main/ChatApplication.java
@@ -1,10 +1,43 @@
 package main;
 
+import java.io.*;
+import java.net.*;
+
 public class ChatApplication {
 
-	public static void main(String[] args) {
-		System.out.println("test commit");
-
+	public static void main(String[] args) throws Exception {
+		
+		
+		ChatApplication ca = new ChatApplication();
+		ca.client();
+	//	new ChatApplication().server();
+	//	ca.client();
 	}
-
+	
+	@SuppressWarnings("resource")
+	public void server() throws Exception{
+		ServerSocket srvSocket = new ServerSocket(50001);
+		Socket socket  = srvSocket.accept();
+		InputStreamReader instrReader = new InputStreamReader(socket.getInputStream());
+		BufferedReader br = new BufferedReader(instrReader);
+		
+		String msg = br.readLine();
+		System.out.println(msg);
+		if(msg!= null){
+			PrintStream prntStream = new PrintStream(socket.getOutputStream());
+			prntStream.println("msg reseived");
+		}
+	}
+	
+	public void client() throws Exception{
+		Socket socket = new Socket("localhost",50001);
+		PrintStream ps = new PrintStream(socket.getOutputStream());
+		ps.println("hello to server from client");
+		
+		InputStreamReader instrReader = new InputStreamReader(socket.getInputStream());
+		BufferedReader br = new BufferedReader(instrReader);
+		
+		String msg = br.readLine();
+		System.out.println(msg);
+	}
 }
---
+++ cs470.chat_application/src/main/TestClient.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.*;
+import java.net.*;
+
+public class TestClient {
+	int i = 0;
+
+	public static void main(String[] args) throws Exception {
+
+		TestClient ca = new TestClient();
+		while (true)
+			ca.client();
+	}
+
+	public void client() throws Exception {
+		Socket socket = new Socket("localhost", 50001);
+		PrintStream ps = new PrintStream(socket.getOutputStream());
+		ps.println("hello to server from client , message : " + ++i);
+
+		InputStreamReader instrReader = new InputStreamReader(socket.getInputStream());
+		BufferedReader br = new BufferedReader(instrReader);
+
+		String msg = br.readLine();
+		System.out.println(msg);
+	}
+}

0ea8a63f8a4cb478e1876e2a3f0bd8885a616106
salem <salem@10.85.57.213>
2016-05-08 20:30:26 -0700

    help

---
+++ cs470.chat_application/src/main/help.java
@@ -0,0 +1,38 @@
+package main;
+
+public class help {
+	
+	public static void main(String[] args) {
+		System.out.println("|****************************HELP MENU***************************|");
+		System.out.println("| 1) help                                                        |");
+		System.out.println("|\tDescription: Display the command options and             |"
+				+ "\n|\t\t     their description.                          |");
+		System.out.println("|----------------------------------------------------------------|");
+		System.out.println("| 2) myip                                                        |");
+		System.out.println("|\tDescription: Display the IP address.                     |");
+		System.out.println("|----------------------------------------------------------------|");
+		System.out.println("| 3) myport                                                      |");
+		System.out.println("|\tDescription: Display listening port.                     |");
+		System.out.println("|----------------------------------------------------------------|");
+		System.out.println("| 4) connect                                                     |");
+		System.out.println("|\tDescription: Establish connection with <destination IP>  |"
+				+"\n|\t\t     using <port number>.                        |");
+		System.out.println("|----------------------------------------------------------------|");
+		System.out.println("| 5) list                                                        |");
+		System.out.println("|\tDescription: Display list of connections.                |");
+		System.out.println("|----------------------------------------------------------------|");
+		System.out.println("| 6) terminate                                                   |");
+		System.out.println("|\tDescription: End connection with IP address              |"
+				+ "\n|\t\t     of <connection id>.                         |");
+		System.out.println("|----------------------------------------------------------------|");
+		System.out.println("| 7) send                                                        |");
+		System.out.println("|\tDescription: Send <message> to IP address                |"
+				+ "\n|\t\t     of <connection id>.                         |");
+		System.out.println("|----------------------------------------------------------------|");
+		System.out.println("| 8) exit                                                        |");
+		System.out.println("|\tDescription: Exit program.                               |");
+		System.out.println("|****************************************************************|");
+	}
+	
+
+}
